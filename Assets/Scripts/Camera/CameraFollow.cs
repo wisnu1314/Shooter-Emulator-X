ed1807dcd89a4a94ce02ad5da2e406bf6c4d0ca3
wisnu1314 <61625865+wisnu1314@users.noreply.github.com>
2022-03-25 09:48:12 +0700

    MigrationFromGitlab

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float smoothing = 10f;
+    Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - target.position;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 targetCamPos = target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+    }
+}
---
+++ Assets/_Complete-Game/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace CompleteProject
+{
+    public class EnemyMovement : MonoBehaviour
+    {
+        Transform player;               // Reference to the player's position.
+        PlayerHealth playerHealth;      // Reference to the player's health.
+        EnemyHealth enemyHealth;        // Reference to this enemy's health.
+        UnityEngine.AI.NavMeshAgent nav;               // Reference to the nav mesh agent.
+
+
+        void Awake ()
+        {
+            // Set up the references.
+            player = GameObject.FindGameObjectWithTag ("Player").transform;
+            playerHealth = player.GetComponent <PlayerHealth> ();
+            enemyHealth = GetComponent <EnemyHealth> ();
+            nav = GetComponent <UnityEngine.AI.NavMeshAgent> ();
+        }
+
+
+        void Update ()
+        {
+            nav.SetDestination (player.position);
+        }
+    }
+}
